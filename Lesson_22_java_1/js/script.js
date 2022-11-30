074909a17b244498aa2cb5f9e2d14d0def485c20
KoskopTB3 <98027113+KoskopTB3@users.noreply.github.com>
2022-11-30 17:30:23 +0200

    123

---
+++ Lesson_22_java_1/js/script.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let sumString = `I learn JS`;
+console.log(sumString);
+
+
+// ================ДЗ №1=====================
+// //Вариант №1
+// console.log('Учим JS')
+
+// //Вариант №2
+// console.log('JS')
+// console.log('Учим')
+
+// //Вариант №3
+// console.log('Учим') console.log('JS')
+
+// //Вариант №4
+// console.log('Учим);
+// console.log('JS');
+
+// ================ДЗ №2=====================
+
+// #1
+let colorEye = `blue`;
+console.log(typeof (colorEye));
+console.log(colorEye);
+
+// #2
+let user;
+let userName = `Вася`;
+user = userName;
+console.log(user);
+
+// ================ДЗ №3=====================
+
+// //Вариант №1
+// let userAge = 36;
+// let userInfo = "Фрилансер ${userAge}";
+
+// //Вариант №2
+// let userHeight = 145 / 0;
+// //Вернется значение NaN
+// console.log(userHeight);
+
+// //Вариант №3
+// let userName;
+// //Вернется тип данных Null
+// console.log(typeof userName);
+
+// //Вариант №4
+// let userSize = "45" / "8";
+// //Вернется тип данных Number
+// console.log(typeof userSize);
